5fdae947a92184ecbf7f51f3095e55dbf78508fc
Arnab Chakraborty <arnabc@webgyani.com>
2011-06-09 01:23:01 +0530

    Added new test case for the actual API, cleaned up issues with POST request

--- test/api-spec.js
+++ test/api-spec.js
@@ -3,7 +3,7 @@ var vows = require('vows'),
     assert = require('assert'),
     EventEmitter = require('events').EventEmitter,
     SafeBrowse = require('../lib/safe_browse'),
-    Config = require('./config.js');
+    Config = require('./config');
 
 
 var apiKey = Config.apikey;
@@ -14,6 +14,26 @@ function createSafeBrowseObj( key, clientName ) {
     return SafeBrowse( key, clientName );    
 }
 
+// Mixed URLs
+var mixedUrls = [
+    'http://quadnode.com',
+    'http://google.com',
+    // do not visit this link, it may harm your computer
+    'http://beatageyer.com/projects/pages/google%2520site%2520check%2520malware.html',
+    'http://benanshell.cz.cc',
+    'http://gumblar.cn',
+    'http://yahoo.com',
+    'http://www.msn.com'
+];
+
+// Good URLs
+var goodUrls = [
+    'http://www.aol.com',
+    'http://www.facebook.com',
+    'http://mint.com',
+    'http://twitter.com'
+];
+
 vows.describe('Safe Browse API')
     .addBatch( {
         'Should always identify the client app for sanity': {
@@ -22,7 +42,7 @@ vows.describe('Safe Browse API')
             'should generate error if client identity not specified': function ( topic ) {
                 var error = 'Client name is required, it helps Google to indentify your Application';
                 assert.throws( function () {
-                    createSafeBrowseObj.apply( exports, topic /* array of apikey and client */ );
+                    createSafeBrowseObj.apply( exports, topic );
                 }, 
                 new RegExp( error ) );
             }
@@ -74,5 +94,108 @@ vows.describe('Safe Browse API')
             }
         }
     } )
-    .export(module);
+    .addBatch( {
 
+        'Test Multiple URLs with Mixed Content': {
+            topic: function () {
+                var sf = createSafeBrowseObj( apiKey, client );
+                sf.lookup( mixedUrls, this.callback );
+            },
+
+            'should be null': function ( error, result ) {
+                assert.isNull( error );
+            },
+
+            'should be an object': function ( error, result ) {
+                assert.isObject( result );
+            },
+
+            'should have the key in the result object': function ( error, result ) {
+                assert.include( result.data, 'http://quadnode.com' );
+            },
+
+            'should be ok': function ( error, result ) {
+                assert.equal( 'ok', result.data['http://quadnode.com'] );
+            },
+
+            'should be malware': function ( error, result ) {
+                assert.equal( 'malware', result.data['http://gumblar.cn'] );
+            },
+
+            'should be 200': function ( error, result ) {
+                assert.equal( 200, result.statusCode );
+            }
+        },
+
+        'Test all good URLs': {
+
+            topic: function () {
+                var sf = createSafeBrowseObj( apiKey, client );
+                sf.lookup( mixedUrls, this.callback );
+            },
+
+            'should be null': function ( error, result ) {
+                assert.isNull( error );
+            },
+
+            'should be an object': function ( error, result ) {
+                assert.isObject( result );
+            },
+
+            'should be ok': function ( error, result ) {
+                assert.equal( 'ok', result.data['http://twitter.com'] );
+            },
+
+            'should be 204': function ( error, result ) {
+                assert.equal( 204, result.statusCode );
+            }
+        },
+
+        'Test GET request using Bad URL': {
+            topic: function () {
+                var sf = createSafeBrowseObj( apiKey, client );
+                sf.lookup( 'http://gumblar.cn', this.callback );
+            },
+
+            'should be null': function ( error, result ) {
+                assert.isNull( error );
+            },
+
+            'should be an object': function ( error, result ) {
+                assert.isObject( result );
+            },
+
+            'should be ok': function ( error, result ) {
+                assert.equal( 'malware', result.data['http://twitter.com'] );
+            },
+
+            'should be 200': function ( error, result ) {
+                assert.equal( 200, result.statusCode );
+            }
+        },
+
+        'Test GET request using Good URL': {
+            topic: function () {
+                var sf = createSafeBrowseObj( apiKey, client );
+                sf.lookup( 'http://twitter.com', this.callback );
+            },
+
+            'should be null': function ( error, result ) {
+                assert.isNull( error );
+            },
+
+            'should be an object': function ( error, result ) {
+                assert.isObject( result );
+            },
+
+            'should be ok': function ( error, result ) {
+                assert.equal( 'ok', result.data['http://twitter.com'] );
+            },
+
+            'should be 204': function ( error, result ) {
+                assert.equal( 204, result.statusCode );
+            }
+        }
+
+    } )
+    .export(module);
